b9b08e3620f506b3f76bfccbc3d2392f842c1ca4
Cloudbop <boppincolin@gmail.com>
2022-01-03 19:01:53 +0000

    basic login ui

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,21 @@
 import React from "react";
-import { DemoApp } from "./DemoCounters/DemoApp";
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">App Header</header>
-      <DemoApp />
-    </div>
-  );
+import { User } from "../model/Model";
+import { AuthService } from "../services/AuthService";
+import { Login } from "./Login";
+
+interface AppState {
+  user: User | undefined;
 }
 
-export default App;
+export class App extends React.Component<{}, AppState> {
+  private authService: AuthService = new AuthService();
+
+  render() {
+    return (
+      <div>
+        App from class works!!!
+        <Login authService={this.authService} />
+      </div>
+    );
+  }
+}
