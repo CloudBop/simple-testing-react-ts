f1831cc7ad91b4e9defd97cf604e82ded38279f3
Cloudbop <boppincolin@gmail.com>
2022-01-03 19:05:27 +0000

    +events to login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { SyntheticEvent } from "react";
 import { AuthService } from "../services/AuthService";
 
 interface LoginProps {
@@ -11,8 +11,11 @@ interface LoginState {
   loginSuccesfull: boolean;
 }
 
+interface CustomEvent {
+  target: HTMLInputElement;
+}
+
 export class Login extends React.Component<LoginProps, LoginState> {
-  // initial state
   state: LoginState = {
     userName: "",
     password: "",
@@ -20,14 +23,44 @@ export class Login extends React.Component<LoginProps, LoginState> {
     loginSuccesfull: false,
   };
 
+  private setUserName(event: CustomEvent) {
+    this.setState({ userName: event.target.value });
+  }
+
+  private setPassword(event: CustomEvent) {
+    this.setState({ password: event.target.value });
+  }
+
+  private async handleSubmit(event: SyntheticEvent) {
+    event.preventDefault();
+    //
+    // access an auth service
+    const result = await this.props.authService.login(
+      this.state.userName,
+      this.state.password
+    );
+    if (result) {
+      console.log(result);
+    } else {
+      console.log("wrong login");
+    }
+  }
+
   render() {
     return (
       <div>
         <h2>Please login</h2>
-        <form>
-          <input value={this.state.userName} />
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <input
+            value={this.state.userName}
+            onChange={(e) => this.setUserName(e)}
+          />
           <br />
-          <input value={this.state.password} type="password" />
+          <input
+            value={this.state.password}
+            onChange={(e) => this.setPassword(e)}
+            type="password"
+          />
           <br />
           <input type="submit" value="Login" />
         </form>
