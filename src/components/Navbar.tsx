8b7e7717ff2c25d0a87857847a09df596695f20d
Cloudbop <boppincolin@gmail.com>
2022-01-05 00:33:26 +0000

    Navrbar + Router Tests

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,7 @@ export class Navbar extends React.Component<{
       );
     } else {
       loginLogOut = (
-        <Link to="/login" style={{ float: "right" }}>
+        <Link to="/login" data-testid="login-link" style={{ float: "right" }}>
           Login
         </Link>
       );
@@ -23,9 +23,18 @@ export class Navbar extends React.Component<{
 
     return (
       <div className="navbar">
-        <Link to="/"> Home</Link>
-        <Link to="/profile"> Profile</Link>
-        <Link to="/spaces"> Spaces</Link>
+        <Link data-testid="home-link" to="/">
+          {" "}
+          Home
+        </Link>
+        <Link data-testid="profile-link" to="/profile">
+          {" "}
+          Profile
+        </Link>
+        <Link data-testid="spaces-link" to="/spaces">
+          {" "}
+          Spaces
+        </Link>
         {loginLogOut}
       </div>
     );
